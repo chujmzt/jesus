632a95c72f79ea2b29be7b51739be56460ccf4a6
LAB02-06 <LAB02-06@DESKTOP-7KA7BRS>
2019-03-04 12:03:27 -0600

    difeniendo clase estudiante y esta en la carpeta modelo

---
+++ unidad lll/unidad lll/modelo/Estudiante.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unidad_lll.modelo
+{
+    class Estudiante
+    {
+        public byte edad { get; set; }
+        public string genero { get; set; }
+        public string nombre { get; set; }
+        public long numeroControl { get; set; }
+
+        public void Estudiar(string texto)
+        {
+            //metodo para estudiar
+            
+        }//fin del metodo estudiar
+
+        public void HacerTarea(string tarea)
+        {
+
+
+            //Metodo que hace tarea
+        }//Fin del metodo hacer tarea
+    }
+}
